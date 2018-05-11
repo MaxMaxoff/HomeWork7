3df931d160c007a731ac8f82789e8d72a6a115af
Maxim VT <maxim.vt@gmail.com>
2018-05-11 13:55:47 +0300

    Add project files.

---
+++ GuessTheNumber/fmMain.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace GuessTheNumber
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btCheck = new System.Windows.Forms.Button();
+            this.txbxLog = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maskedTBNumber = new System.Windows.Forms.MaskedTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Угадай число:";
+            // 
+            // btCheck
+            // 
+            this.btCheck.Enabled = false;
+            this.btCheck.Location = new System.Drawing.Point(121, 38);
+            this.btCheck.Name = "btCheck";
+            this.btCheck.Size = new System.Drawing.Size(75, 23);
+            this.btCheck.TabIndex = 2;
+            this.btCheck.Text = "Пробуем";
+            this.btCheck.UseVisualStyleBackColor = true;
+            this.btCheck.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txbxLog
+            // 
+            this.txbxLog.Location = new System.Drawing.Point(15, 66);
+            this.txbxLog.Multiline = true;
+            this.txbxLog.Name = "txbxLog";
+            this.txbxLog.Size = new System.Drawing.Size(181, 142);
+            this.txbxLog.TabIndex = 3;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(207, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Text = "Новая";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Выход";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // maskedTBNumber
+            // 
+            this.maskedTBNumber.Location = new System.Drawing.Point(15, 40);
+            this.maskedTBNumber.Mask = "000";
+            this.maskedTBNumber.Name = "maskedTBNumber";
+            this.maskedTBNumber.Size = new System.Drawing.Size(100, 20);
+            this.maskedTBNumber.TabIndex = 5;
+            this.maskedTBNumber.ValidatingType = typeof(int);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(207, 217);
+            this.Controls.Add(this.maskedTBNumber);
+            this.Controls.Add(this.txbxLog);
+            this.Controls.Add(this.btCheck);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Угадай число";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btCheck;
+        private System.Windows.Forms.TextBox txbxLog;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MaskedTextBox maskedTBNumber;
+    }
+}
+
---
+++ HomeWork7/fmMain.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Максим Торопов
+/// Ярославль
+/// Домашняя работа 7 урока
+/// 
+/// а) Добавить в программу “Удвоитель” подсчет количества отданных команд удвоителю.
+/// б) Добавить меню и команду “Играть”. При нажатии появляется сообщение, какое число должен получить игрок.
+/// Игрок должен постараться получить это число за минимальное количество ходов.
+/// в) * Добавить кнопку “Отменить”, которая отменяет последние ходы.
+/// </summary>
+namespace HomeWork7
+{
+    public partial class fmMain : Form
+    {
+        Doubler t;
+
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            t = new Doubler();
+            lbFinish.Text = $"Цель: {t.Finish.ToString()}";
+
+            MessageBox.Show(lbFinish.Text);
+
+            txbxLog.Text = String.Empty;   
+            txbxLog.AppendText(t.Log("NewGame"));
+            UpdateGameStatus();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            t.Increment();
+
+            txbxLog.AppendText(Environment.NewLine);
+            txbxLog.AppendText(t.Log("Plus"));
+            UpdateGameStatus();
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            t.Doubling();
+
+            txbxLog.AppendText(Environment.NewLine);
+            txbxLog.AppendText(t.Log("Doubling"));
+            UpdateGameStatus();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            t.Reset();
+
+            txbxLog.AppendText(Environment.NewLine);
+            txbxLog.AppendText(t.Log("Reset"));
+            UpdateGameStatus();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (t.Steps > 0)
+            {
+                // Remove last line
+                txbxLog.Text = txbxLog.Text.Remove(txbxLog.Text.LastIndexOf(Environment.NewLine));
+
+                // StepBack using last line
+                t.Back(txbxLog.Lines.GetValue(txbxLog.Lines.Length - 1).ToString());
+
+                UpdateGameStatus();
+            };
+        }
+
+        private void UpdateGameStatus()
+        {
+            lbCurrent.Text = $"Текущее: {t.Current.ToString()}";
+            lbStep.Text = $"Шаг: {t.Steps.ToString()}";
+
+            if (t.Current > t.Finish)
+            {
+                btnPlus.Enabled = false;
+                btnMulti.Enabled = false;
+                btnReset.Enabled = false;
+                btnBack.Enabled = false;
+                MessageBox.Show("Вы проиграли!");
+            }
+            if (t.Current == t.Finish)
+            {
+                btnPlus.Enabled = false;
+                btnMulti.Enabled = false;
+                btnReset.Enabled = false;
+                btnBack.Enabled = false;
+                MessageBox.Show("Вы выиграли!");
+            }
+            if (t.Current < t.Finish)
+            {
+                btnPlus.Enabled = true;
+                btnMulti.Enabled = true;
+                btnReset.Enabled = true;
+                btnBack.Enabled = true;
+            }
+        }
+    }
+}
